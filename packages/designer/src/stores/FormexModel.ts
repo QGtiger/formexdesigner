ac5cb8b1a99874b1b329c4c9a227b1aa2e804a4c
光鱼 <guang.yu@yingdao.com>
2025-07-17 10:49:11 +0800

    feat: 更新版本号至1.0.15-beta.14，优化代码格式和国际化支持

--- packages/designer/src/stores/FormexModel.ts
+++ packages/designer/src/stores/FormexModel.ts
@@ -1,4 +1,5 @@
-import i18next from 'i18next';import { createCustomModel } from "@/common/createModel";
+import i18next from "i18next";
+import { createCustomModel } from "@/common/createModel";
 import { useBoolean } from "ahooks";
 import { Form } from "antd";
 
@@ -8,6 +9,11 @@ export interface FormexModelProps {
   successText?: string;
   showErrorMessage?: (text: string) => void;
   onFileUpload?: (file: File) => Promise<string>;
+
+  bannerList?: {
+    value: string;
+    label: string;
+  }[];
 }
 
 export const FormexModel = createCustomModel((props: FormexModelProps) => {
@@ -28,9 +34,9 @@ export const FormexModel = createCustomModel((props: FormexModelProps) => {
   };
 
   const handleImageUpload = async (
-  file: File,
-  callback: (urL: string, desc: string) => void) =>
-  {
+    file: File,
+    callback: (urL: string, desc: string) => void
+  ) => {
     try {
       if (!onFileUpload) {
         throw new Error("onFileUpload function is not provided");
@@ -40,8 +46,8 @@ export const FormexModel = createCustomModel((props: FormexModelProps) => {
       // 4. 插入编辑器
       callback(url, file.name || "image.png");
     } catch (error) {
-      console.error(i18next.t('intl37'), error);
-      callback("", i18next.t('intl38'));
+      console.error(i18next.t("intl37"), error);
+      callback("", i18next.t("intl38"));
     }
   };
 
@@ -51,6 +57,6 @@ export const FormexModel = createCustomModel((props: FormexModelProps) => {
     showSuccessPage,
     onReFill,
     onFormexFinish,
-    handleImageUpload
+    handleImageUpload,
   };
 });
